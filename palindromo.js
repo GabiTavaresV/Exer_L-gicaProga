a1de4b5c991f09a92f4f5d1d5678d5cb4d42382a
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-12-06 19:41:38 -0300

    :star2: Exercício Palíndromo

---
+++ palindromo.js
@@ -0,0 +1,12 @@
+      function pali(str){
+
+        let reversedStr = str.split("").reverse().join("");
+        if(str === reversedStr){
+          console.log( `A palavra ${str} é um palindromo`)
+        } else{
+          console.log(" não é um palindromo")
+        }
+           
+      }
+
+   (pali("reviver"));
